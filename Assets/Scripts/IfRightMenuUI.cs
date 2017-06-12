f740ced033728dcb39b2c63957e79173083fe556
ErnestoJeon <ernesto1026@daum.net>
2017-06-12 16:30:33 +0900

    테스트 중

---
+++ Assets/Scripts/IfRightMenuUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IfRightMenuUI : MonoBehaviour
+{
+    public GameObject _shopButton;
+    public GameObject _cardButton;
+    public GameObject _battleButton;
+
+    public void Update()
+    {
+        if (InputField.
+        {input
+            Instantiate(_shopButton, this.transform.position, transform.rotation);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Instantiate(_cardButton, this.transform.position, transform.rotation);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Instantiate(_battleButton, this.transform.position, transform.rotation);
+        }
+    }
+    public void MenuOpen()
+    {
+        gameObject.SetActive(true);
+    }
+    public void MenuClose()
+    {
+        gameObject.SetActive(false);
+    }
+}
